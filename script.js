d7ce58e4d0a3c4d54a6c42d07a5bdccd37713946
Besim Mustafi <besimnaim@gmail.com>
2023-11-03 16:26:27 +0100

    Udemy_Video 21

--- script.js
+++ script.js
@@ -28,4 +28,29 @@ if (heighestBMI) {console.log(`Mark's BMI is higher then John's ${bmiMark}`);
     
 }
 
-//Udemy - Video 21 open
+//Udemy - Video 21
+// falsy values
+
+console.log(Boolean(0));            //false
+console.log(Boolean(undefined));    //false
+console.log(Boolean(`Besim`));      //true
+console.log(Boolean());             //false
+console.log(Boolean({}));           //true
+
+
+let money = 0;  //The output will be "False in the If function -> jump to the else function"
+if (money) {
+    console.log(`Gönn dir, du hast Geld`);
+}
+else {console.log(`Du hast kein Geld, such dir ein Job`);
+}
+
+let height;  //The variable "height" is UNDEFINED -> The output will be "False"
+if (money) {
+    console.log(`Variable is defined`);
+}
+else {console.log(`The variable is undefined, please define 'height'`);
+}
+
+
+//Udemy - Video 22
