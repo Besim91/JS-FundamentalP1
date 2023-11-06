77a58de8d75ebe2f4e51e2accc5127d2933baeeb
Besim Mustafi <besimnaim@gmail.com>
2023-11-06 11:30:59 +0100

    Udemy_Video 24

--- script.js
+++ script.js
@@ -53,4 +53,37 @@ else {console.log(`The variable is undefined, please define 'height'`);
 }
 
 
-//Udemy - Video 22
+/*Udemy - Video 24
+There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner 
+with the highest average score wins a trophy!
+Your tasks:
+
+1. Calculate the average score for each team, using the test data included below. The average score 
+for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+"Dolphins win the trophy" if Dolphins win, or
+"Koalas win the trophy" if Koalas win, or
+"Both win the trophy" if their average scores are equal.
+
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.*/
+
+const firstScoreDolphins = 96;
+const secondScoreDolphins = 108;
+const thirdScoreDolphins = 89;
+
+const firstScoreKoalas = 88;
+const secondScoreKoalas = 91;
+const thirdScoreKoalas = 110;
+
+let rounds = 3;
+
+let averageScoreDolphins = ((firstScoreDolphins + secondScoreDolphins + thirdScoreDolphins) / rounds).toFixed(2);
+let averageScoreKoalas = ((firstScoreKoalas + secondScoreKoalas + thirdScoreKoalas) / rounds).toFixed(2);
+
+console.log(`Average score Dolphins: ${averageScoreDolphins}` ,`Average score Koalas: ${averageScoreKoalas}` );
+
+if (averageScoreKoalas > averageScoreDolphins)
+{console.log(`Koalas win the trophy`)}
+else if (averageScoreKoalas < averageScoreDolphins) {console.log(`Dolphins win the trophy`)}
+else if (averageScoreDolphins === averageScoreKoalas){ console.log(`Both win the trophy`);}
